cfa2f3c88330f079385ce4855bfe4d5016f0e881
kekeszniki <156064153+kekeszniki@users.noreply.github.com>
2024-03-28 11:40:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const startBtn = document.querySelector('.start-btn');
+const exitBtn = document.querySelector('.exit-btn');
+const main = document.querySelector('.main');
+const continueBtn = document.querySelector('.continue-btn');
+const quizSection = document.querySelector('.quiz-section');
+const quizBox = document.querySelector('.quiz-box');
+const resultBox = document.querySelector('.result-box');
+const tryAgainBtn = document.querySelector('.tryAgain-btn');
+const goHomeBtn = document.querySelector('.goHome-btn');
+
+startBtn.onclick = () => {
+	quizSection.classList.add('active');
+	
+	
+}
